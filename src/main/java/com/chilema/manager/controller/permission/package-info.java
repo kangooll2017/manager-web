78cb73cb4e2599ec208020b9b5da11b30c05e044
kangooll <328801750@qq.com>
2017-08-08 21:05:59 +0800

    框架搭设完成，初次提交

---
+++ src/main/java/com/chilema/manager/controller/permission/package-info.java
@@ -0,0 +1,37 @@
+/******************************************************************
+ *
+ *    Java Lib For Android, Powered By Shenzhen Jiuzhou.
+ *
+ *    Copyright (c) 2001-2014 Digital Telemedia Co.,Ltd
+ *    http://www.d-telemedia.com/
+ *
+ *    Package:     com.chilema.manager.controller.permission
+ *
+ *    Filename:    package-info.java
+ *
+ *    Description: TODO(用一句话描述该文件做什么)
+ *
+ *    Copyright:   Copyright (c) 2001-2014
+ *
+ *    Company:     Digital Telemedia Co.,Ltd
+ *
+ *    @author:     Kang
+ *
+ *    @version:    1.0.0
+ *
+ *    Create at:   2017年8月8日 下午7:03:40
+ *
+ *    Revision:
+ *
+ *    2017年8月8日 下午7:03:40
+ *        - first revision
+ *
+ *****************************************************************/
+/**
+ * @ClassName package-info
+ * @Description TODO(这里用一句话描述这个类的作用)
+ * @author Kang
+ * @Date 2017年8月8日 下午7:03:40
+ * @version 1.0.0
+ */
+package com.chilema.manager.controller.permission;
